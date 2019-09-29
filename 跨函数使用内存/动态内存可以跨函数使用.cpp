68d91bd828af89e2dea821506f8ac64ecac440ca
WLthunder的储物间 <55611520+Wlthunder@users.noreply.github.com>
2019-09-29 20:24:16 +0800

    8_跨函数使用内存

---
+++ 跨函数使用内存/动态内存可以跨函数使用.cpp
@@ -0,0 +1,23 @@
+# include <stdio.h>
+# include <malloc.h>
+
+void f(int ** q)
+{
+	*q = (int *)malloc(sizeof(int));	// sizeof(数据类型)返回值是该数据类型所占的字节数
+										// 这样写可移植性很强
+										// 等价于 p = (int *)malloc(sizeof(int));
+//	q = 5;								// error
+//	*q = 5;                             // p = 5;
+	**q = 5; 	                        // *p = 5;
+}
+
+int main(void)
+{
+	int * p;
+	
+	f(&p);
+//	free(p);
+	printf("%d\n", *p);
+	
+	return 0;
+}
